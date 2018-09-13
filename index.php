264d16f78b871853b01f1b57f6acb6fd03c6b5a1
Jerome Graf <jerome.graf@sap.com>
2018-09-13 17:38:43 +0200

    Auswahl der Sprache gelöscht

--- index.php
+++ index.php
@@ -81,8 +81,6 @@
 <body>
     <p><img src="MAMP-PRO-Logo.png" id="logo" alt="MAMP PRO Logo" width="250" height="49" /></p>
 
-<?php if ($str_language == 'de'): ?>
-
     <p class="text"><strong>Der virtuelle <span lang="en" xml:lang="en">Host</span> wurde erfolgreich eingerichtet.</strong></p>
     <p class="text">Wenn Sie diese Seite sehen, dann bedeutet dies, dass der neue virtuelle <span lang="en" xml:lang="en">Host</span> erfolgreich eingerichtet wurde. Sie können jetzt Ihren <span lang="en" xml:lang="en">Web</span>-Inhalt hinzufügen, diese Platzhalter-Seite<sup><a href="#footnote_1">1</a></sup> sollten Sie ersetzen <abbr title="beziehungsweise">bzw.</abbr> löschen.</p>
     <p class="text">
@@ -91,6 +89,5 @@
     </p>
     <p class="text" id="footnote_1"><small><sup>1</sup> Dateien: <samp>index.php</samp> und <samp>MAMP-PRO-Logo.png</samp></small></p>
     <hr />
-    <p class="text">This page in: <?php echo $str_available_languages; ?></p>
 </body>
 </html>
