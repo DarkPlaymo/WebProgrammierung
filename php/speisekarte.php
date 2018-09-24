ccb6dab8e8726e195fa5b03b764921e8916f2e7f
Jerome Graf <jerome.graf@sap.com>
2018-09-24 10:24:06 +0200

    Verbesserung Speisekarte

--- php/speisekarte.php
+++ php/speisekarte.php
@@ -5,38 +5,32 @@
     </head>
 
     <body>  
-        <?php $shop = array(array("rose",   1.25, 15),
-                            array("daisy",  0.75, 25),
-                            array("orchid", 1.15, 7 ),);
-                            
-            //nachschauen ob Count so stimmt
+        <?php 
             $pdo = new PDO('mysql:host=localhost; dbname=_____', 'username', 'password');
 
-            $SQLUntertypen =  "SELECT Untertypen FROM Speisen WHERE typ=$_SESSION['type']" ?> 
+            $SQLUntertypen =  "SELECT Untertypen FROM Speisen WHERE typ=$_SESSION['type']";
             
-        
-        <h2> <?php echo $_SESSION["type"] ?> </h2>
-
-
-        
-        <?php foreach ($pdo->query($SQLUntertypen) as $Untertyp) : 
-            $SQLProdukte = "SELECT Produkt, Preis FROM Speisen WHERE typ=$_SESSION[type] AND untertyp=$Untertyp" ?>
+            foreach ($pdo->query($SQLUntertypen) as $Untertyp) : 
+                $SQLProdukte = "SELECT Produkt, Preis FROM Speisen WHERE typ=$_SESSION[type] AND untertyp=$Untertyp" 
+        ?>
 
             <!--Anfang accordeon mit title $Untertypname-->
-
+            
+                <h2> <?php echo $_SESSION["type"] ?> </h2> <!-- nur vorübergehend-->
                 <table>
                     <tr><td>Produkt</td><td>Preis</td></tr>
 
                     <?php foreach ($pdo->query($SQLProdukte) as $row) : ?>
                         <tr>
-                            <td><?php echo $row[0]; ?></td>
-                            <td><?php echo $row[1]; ?></td>
+                            <td><?php echo $row['Produkt']; ?></td>
+                            <td><?php echo $row['Preis']; ?></td>
                         </tr>
                     <?php endforeach; ?>
 
                 </table>
 
             <!--Ende Accordeon -->
+
         <?php endforeach; ?>
     </body>
 </html>
