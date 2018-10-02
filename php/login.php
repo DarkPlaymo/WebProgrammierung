9af632ef666b64d144ef60c425b0b662759d590d
lionduke97 <43246229+lionduke97@users.noreply.github.com>
2018-10-02 10:43:12 +0200

    Login

--- php/login.php
+++ php/login.php
@@ -7,6 +7,36 @@
     </head>
 
     <body>
-        <!-- hier Code einfüge -->
+
+        <h2>Anmeldeformular</h2>
+
+        <form action="bestellen.php">
+        Nachname, Vorname*:<br>
+        <input type="text" name="name" value="">
+        <br><br>
+        Anzahl der Personen*:<br>
+        <input type="number" name="number" min="1" max="10" value="">
+        <br><br>
+        Wo möchten Sie sitzen?*:<br>
+        <div class="small-12 medium-6 large-6 columns">
+            <span class="wpcf7-form-control-wrap Tischauswahl">
+                <select name="Tischauswahl" class="wpcf7-form-control wpcf7-select" id="Tischauswahl" aria-invalid="false">
+                    <option value="Keine Angabe">Keine Angabe</option>
+                    <option value="Tisch unten">Tisch unten</option>
+                    <option value="Tisch oben">Tisch oben</option>
+                    <option value="Terrasse">Terrasse</option>
+                </select>
+            </span>
+        </div>
+        <br>
+        Nachricht:<br>
+        <input type="text" name="message" value="">
+        <br><br>
+        
+        <input type="submit" value="Submit">
+        </form> 
+
+        <p>If you click the "Submit" button, the form-data will be sent to a page called "bestellen.php".</p>
+
     </body>
 </html> 
